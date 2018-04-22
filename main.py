0111dd2e062170bf759379e421515b1c4c31e69d
Rigel Di Scala <zedr@zedr.com>
2018-04-22 11:43:36 +0200

    Sun Apr 22 11:43:36 CEST 2018

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
 #!/usr/bin/env python3
+
+def main():
+    while True:
+        cmd, *args = input("? ").split(" ")
+        if cmd.lower() == "say":
+            print(" ".join(args))
+        else:
+            print("Unknown command: {}".format(cmd))
+
+if __name__ == "__main__":
+    main()
